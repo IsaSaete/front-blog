6bb36345d313f547c66716acd3d6fa5df27ae82c
Isabel Sáenz <isasaete@gmail.com>
2025-04-29 20:12:48 +0200

    Add test to PostDetail Page

---
+++ src/post/pages/PostDetailPage/PostDetailPage.test.tsx
@@ -0,0 +1,48 @@
+import { MemoryRouter, Route, Routes } from "react-router";
+import Layout from "../../../components/Layout/Layout";
+import { render, screen } from "@testing-library/react";
+import PostsContextProvider from "../../context/PostsContextProvider";
+import PostDetailPage from "./PostDetailPage";
+import { huevosRotosBruc159PostDto } from "../../dto/fixturesDto";
+
+describe("Given the PostDetailPage component", () => {
+  describe("When it receives 159678901234567890123456 id", () => {
+    test("Then it should show 'Huevos Rotos de Bruc, 159' inside a heading", async () => {
+      render(
+        <PostsContextProvider>
+          <MemoryRouter initialEntries={["/post/159678901234567890123456"]}>
+            <Layout />
+            <Routes>
+              <Route path="post/:id" element={<PostDetailPage />} />
+            </Routes>
+          </MemoryRouter>
+        </PostsContextProvider>,
+      );
+
+      const postTitle = await screen.findByRole("heading", {
+        name: new RegExp(huevosRotosBruc159PostDto.title, "i"),
+      });
+
+      expect(postTitle).toBeVisible();
+    });
+
+    test("Then it should show an image of 'Huevos rotos servidos en un plato rústico en Bruc 159'", async () => {
+      render(
+        <PostsContextProvider>
+          <MemoryRouter initialEntries={["/post/159678901234567890123456"]}>
+            <Layout />
+            <Routes>
+              <Route path="post/:id" element={<PostDetailPage />} />
+            </Routes>
+          </MemoryRouter>
+        </PostsContextProvider>,
+      );
+
+      const postDetailImage = await screen.findByAltText(
+        new RegExp(huevosRotosBruc159PostDto.imageAlt, "i"),
+      );
+
+      expect(postDetailImage).toBeVisible();
+    });
+  });
+});
